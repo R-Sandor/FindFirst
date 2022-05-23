15c1e7183e41c8faa5b607023c2ab8ab9a2a7459
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2022-05-23 18:17:00 -0400

    updated to java version 17

--- server/src/main/java/dev/renegade/bookmarkit/controller/BookmarkController.java
+++ server/src/main/java/dev/renegade/bookmarkit/controller/BookmarkController.java
@@ -1,10 +1,8 @@
 package dev.renegade.bookmarkit.controller;
 
 import java.util.List;
-import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,30 +49,3 @@ public class BookmarkController {
     }
 
 }
-
-// @Autowired
-// private BookmarkRepository bookmarkRepository;
-
-// @GetMapping("/bookmarks/{bookmarkId}/tags")
-// public ResponseEntity<List<Tag>> getAllCommentsByTutorialId(@PathVariable(value = "bookmarkId")
-// Long bookmarkId) {
-// System.out.println("HERE");
-// if (!bookmarkRepository.existsById(bookmarkId)) {
-// throw new ResourceNotFoundException("Not found Tutorial with id = " + bookmarkId);
-// }
-// System.out.println(bookmarkId);
-
-// List<Tag> tags = tagRepository.findByBookmarkId(bookmarkId);
-// System.out.println(tags);
-// return new ResponseEntity<>(tags, HttpStatus.OK);
-// }
-
-// @GetMapping("/tags/{id}")
-// public ResponseEntity<Tag> getTagByBookmarkId(@PathVariable(value = "id") Long id) {
-// Tag tag = tagRepository.findById(id)
-// .orElseThrow(() -> new ResourceNotFoundException("Not found Comment with id = " + id));
-
-// return new ResponseEntity<>(tag, HttpStatus.OK);
-// }
-
-
