b9d2092ac5d984c9b0f2d5e5b91c9d9c065041dc
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-07-06 17:45:46 -0400

    Checking in the old vue code

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from './plugins/font-awesome'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App)
   .use(router)
