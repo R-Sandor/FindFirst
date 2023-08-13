1e388a43bf497761ca464593a4a5624a8149c82a
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-13 10:24:17 -0400

    Data is working!

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,24 @@
 "use client";
-import  authService, { AuthStatus } from "@/services/auth.service";
+import authService, { AuthStatus } from "@/services/auth.service";
+import TagList from '@components/TagList'
 import { useEffect, useState } from "react";
+
 export default function App() {
   const [authorized, setAuthorized] = useState<AuthStatus>();
 
   useEffect(() => {
-    setAuthorized(authService.getAuthorized())
-}, []); 
+    setAuthorized(authService.getAuthorized());
+  }, []);
 
-  /** 
+  /**
    * Ideally when the user visits the site they will actually have a cool landing page
-   * rather than redirecting them immediately to sign in. 
-   * Meaning that the '/' will eventually be added to the public route and not authenticated will be the 
-   * the regular landing. 
-  */
-  return(  authorized ? <div> My bookmarks and cool stuff. </div> : <div> Hello Welcome to BookmarkIt. </div>);
+   * rather than redirecting them immediately to sign in.
+   * Meaning that the '/' will eventually be added to the public route and not authenticated will be the
+   * the regular landing.
+   */
+  return authorized ? <div>
+    <div className="w-1/4">
+    <TagList/>
+    </div>
+  </div> : <div> Hello Welcome to BookmarkIt. </div>;
 }
