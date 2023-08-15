23d4be8e469bd076b8f1a326ac209d5bee4ad1ba
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-15 06:44:41 -0400

    Moved typescript to own directory

--- frontend/components/TagList.tsx
+++ frontend/components/TagList.tsx
@@ -1,50 +1,46 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import Badge from "react-bootstrap/Badge";
 import ListGroup from "react-bootstrap/ListGroup";
-import Bookmark from "@components/Bookmarks/Bookmark";
-import Tag from "@components/Bookmarks/Tag";
+import Bookmark from "@/types/Bookmarks/Bookmark";
+import Tag from "@/types/Bookmarks/Tag";
 import api from "@api/Api";
 
 interface TagProp {
-  tags: Tag[]
+  tags: Tag[];
 }
 
-
-const Tags = ({ tags }:TagProp) => {
-  return  (
+const Tags = ({ tags }: TagProp) => {
+  return (
     <div>
-    <ListGroup>
-      {tags.map(tag =>
-        <ListGroup.Item key={tag.id}>
-           {tag.title}
-        </ListGroup.Item>
-      )}
-    </ListGroup>
+      <ListGroup>
+        {tags.map((tag) => (
+          <ListGroup.Item key={tag.id}>{tag.title}</ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
-  )
-
-}
+  );
+};
 
 const TagList: React.FC = () => {
   const [tags, setTags] = useState<Tag[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      let list: Tag [] = [];
-      api.getAllTags().then((response) => {
+    let list: Tag[] = [];
+    api.getAllTags().then((response) => {
       for (let tag of response.data) {
-        list.push(tag)
+        list.push(tag);
       }
+      setLoading(false);
     });
     setTags(list);
-    console.log(list)
+    console.log(list);
   }, []);
 
-  return (
-    <div>
-    {Tags({tags: tags})}
-    </div>
-  );
+  if (loading) return <p> Loading Data</p>;
+
+  return <div>{Tags({ tags: tags })}</div>;
 };
 
 export default TagList;
