820bc0fc3eb54d1a3ec68655efb43e7ed84f2c29
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-16 06:59:34 -0400

    Added some more default bookmarks for testing

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,11 +1,15 @@
-insert into bookmark (id, title, url) values(1, 'test', 'test.com');
-insert into bookmark values(2, 'test', 'test.com');
+insert into bookmark (id, title, url) values(1, 'Best Cheesecake Recipe', 'https://sugarspunrun.com/best-cheesecake-recipe/');
+insert into bookmark values(2, 'Dark mode guide', 'https://blog.logrocket.com/dark-mode-react-in-depth-guide/');
 --insert into bookmark values(3, "test", "test.com");
 --insert into bookmark values(4, "test", "test.com");
 
-insert into tag(id, tag_title) values(1, 'TESTING');
+insert into tag(id, tag_title) values(1, 'Cooking');
+insert into tag(id, tag_title) values(2, 'web_dev');
 
 insert into bookmark_tag values(1,1);
+
+insert into bookmark_tag values(2,2);
+
 insert into roles(id, name) values(1, 'ROLE_USER');
 -- Password is test
 insert into users(id, username, name, email, password) values(1, 'jsmith', 'John Smith', 'jsmith@google.com', '$2a$10$uhmwgpRI0vxF51s8pxt94Ojs8Cwrg7uLwhf3sK7EYv1i7QLML7aJ6');
