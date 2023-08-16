d6062157ad179742a30c9ab1ecdf0db9b9ddc617
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-16 07:02:52 -0400

    More changes for dummy data.

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,4 +1,5 @@
 insert into bookmark (id, title, url) values(1, 'Best Cheesecake Recipe', 'https://sugarspunrun.com/best-cheesecake-recipe/');
+insert into bookmark (id, title, url) values(3, 'Best Cheesecake Recipe', 'https://www.foodnetwork.com/recipes/bobby-flay/chicken-parmigiana-recipe-1952359');
 insert into bookmark values(2, 'Dark mode guide', 'https://blog.logrocket.com/dark-mode-react-in-depth-guide/');
 --insert into bookmark values(3, "test", "test.com");
 --insert into bookmark values(4, "test", "test.com");
@@ -6,11 +7,13 @@ insert into bookmark values(2, 'Dark mode guide', 'https://blog.logrocket.com/da
 insert into tag(id, tag_title) values(1, 'Cooking');
 insert into tag(id, tag_title) values(2, 'web_dev');
 
+-- Cooking related bookmark_tags
 insert into bookmark_tag values(1,1);
-
+insert into bookmark_tag values(1,3);
+-- tech related bookmark_tags
 insert into bookmark_tag values(2,2);
 
 insert into roles(id, name) values(1, 'ROLE_USER');
--- Password is test
+-- login is jsmith/test
 insert into users(id, username, name, email, password) values(1, 'jsmith', 'John Smith', 'jsmith@google.com', '$2a$10$uhmwgpRI0vxF51s8pxt94Ojs8Cwrg7uLwhf3sK7EYv1i7QLML7aJ6');
 -- insert into user_roles values(1,1);
