04cb7c2e244e939880a5a10c376a5af724b4d4a2
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-18 17:47:55 -0400

    id to user_id

--- server/src/main/java/dev/findfirst/bookmarkit/security/service/UserDetailsImpl.java
+++ server/src/main/java/dev/findfirst/bookmarkit/security/service/UserDetailsImpl.java
@@ -43,7 +43,7 @@ public class UserDetailsImpl implements UserDetails {
             .collect(Collectors.toList());
 
     return new UserDetailsImpl(
-        user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), authorities);
+        user.getUserId(), user.getUsername(), user.getEmail(), user.getPassword(), authorities);
   }
 
   @Override
