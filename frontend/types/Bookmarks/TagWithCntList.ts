0e547cb2379689ccb563248d97a7d2649a4e733c
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-18 12:07:15 -0400

    added types

---
+++ frontend/types/Bookmarks/TagWithCntList.ts
@@ -0,0 +1,7 @@
+import Tag from "./Tag";
+import TagWithCnt from "./TagWithCnt";
+
+// List of Tags that contain their count.
+export default interface TagWithCntList  {
+    tagsCounted: TagWithCnt []
+}
