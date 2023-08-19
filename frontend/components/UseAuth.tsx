a96a75a9c77deae3b83a69f06e8e7b9d24551144
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-19 04:29:22 -0400

    Set intial state to unauthorized.

--- frontend/components/UseAuth.tsx
+++ frontend/components/UseAuth.tsx
@@ -2,7 +2,7 @@ import authService, { AuthObserver, AuthStatus } from "@services/auth.service";
 import { useEffect, useState } from "react";
 
 export default function UseAuth() {
-  const [authorized, setAuthorized] = useState<AuthStatus>();
+  const [authorized, setAuthorized] = useState<AuthStatus>(AuthStatus.Unauthorized);
 
   const onAuthUpdated: AuthObserver = (authState: AuthStatus) => {
     setAuthorized(authState);
