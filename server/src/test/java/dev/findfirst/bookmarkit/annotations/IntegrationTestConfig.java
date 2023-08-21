0df59257a16281c930eb44e883586ac71e290f51
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-21 08:10:14 -0400

    Fixed annotation

--- server/src/test/java/dev/findfirst/bookmarkit/annotations/IntegrationTestConfig.java
+++ server/src/test/java/dev/findfirst/bookmarkit/annotations/IntegrationTestConfig.java
@@ -1,8 +1,16 @@
 package dev.findfirst.bookmarkit.annotations;
 
-import org.junit.jupiter.api.condition.EnabledIf;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 
-@EnabledIf(value = "#{environment.getActiveProfiles()[0] == 'integration'}")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@EnabledIf(
+    value = "#{{'test', 'prod'}.contains(environment.getActiveProfiles()[0])}",
+    loadContext = true)
 @SpringBootTest
 public @interface IntegrationTestConfig {}
--- server/src/test/java/dev/findfirst/bookmarkit/service/TagServiceTest.java
+++ server/src/test/java/dev/findfirst/bookmarkit/service/TagServiceTest.java
@@ -3,13 +3,8 @@ package dev.findfirst.bookmarkit.service;
 import dev.findfirst.bookmarkit.annotations.IntegrationTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.EnabledIf;
 
-// import org.junit.jupiter.api.Test;
 @IntegrationTestConfig
-@EnabledIf(
-    value = "#{{'test', 'prod'}.contains(environment.getActiveProfiles()[0])}",
-    loadContext = true)
 public class TagServiceTest {
   @Autowired private TagService tagService;
 
