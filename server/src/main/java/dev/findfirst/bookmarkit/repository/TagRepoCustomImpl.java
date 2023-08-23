e040a54c272393fce8aa7fe9e779ea5cd0514c58
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-08-23 04:58:13 -0400

    Modified query to ensure that discriminator was applied

--- server/src/main/java/dev/findfirst/bookmarkit/repository/TagRepoCustomImpl.java
+++ server/src/main/java/dev/findfirst/bookmarkit/repository/TagRepoCustomImpl.java
@@ -3,10 +3,12 @@ package dev.findfirst.bookmarkit.repository;
 import dev.findfirst.bookmarkit.model.Bookmark;
 import dev.findfirst.bookmarkit.model.Tag;
 import dev.findfirst.bookmarkit.model.TagCntRecord;
+import dev.findfirst.bookmarkit.security.contexts.TenantContext;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +19,16 @@ public class TagRepoCustomImpl implements TagRepoCustom {
 
   @Autowired EntityManager em;
 
+  @Autowired private TenantContext tenantContext;
+  
   @Override
   public List<TagCntRecord> customTagsWithCnt() {
     CriteriaBuilder qb = em.getCriteriaBuilder();
     CriteriaQuery<TagCntRecord> cq = qb.createQuery(TagCntRecord.class);
     Root<Bookmark> bkmk = cq.from(Bookmark.class);
     Join<Bookmark, Tag> tag_bookmark = bkmk.join("tags");
-    cq.multiselect(bkmk.get("tags"), qb.count(bkmk)).groupBy(tag_bookmark, bkmk.get("tags"));
+    cq.multiselect(bkmk.get("tags"), qb.count(bkmk)).groupBy(tag_bookmark, bkmk.get("tags"))
+      .where(qb.equal(bkmk.get("tenantId"), tenantContext.getTenantId()));
     return em.createQuery(cq).getResultList();
   }
 }
