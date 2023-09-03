47dae6deb222e96b4bb1cbb9b49a3530bf11d075
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-09-03 07:02:19 -0400

    Simple bookmark group

--- frontend/components/bookmark/BookmarkGroup.tsx
+++ frontend/components/bookmark/BookmarkGroup.tsx
@@ -1,1 +1,25 @@
+import Bookmark from "@/types/Bookmarks/Bookmark";
+import BookmarkCard from "./BookmarkCard";
+
 // Bookmark group composed of Bookmarks.
+export default function BookmarkGroup({
+  bookmarks,
+}: {
+  bookmarks: Bookmark[];
+}) {
+
+  let bookmarkGroup: any = [];
+  bookmarks.map((b, i) => {
+    bookmarkGroup.push(
+      <div key={i} className="col-6 col-md-12 col-lg-4">
+        <BookmarkCard bookmark={b} />
+      </div>
+    )
+  })
+
+  return (
+    <div className="row no-pad">
+        { bookmarkGroup } 
+    </div>
+  );
+}
