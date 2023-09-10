eafc9a43a5830bad94e3a90cf7013b92cd474e3b
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-09-10 07:44:27 -0400

    More clean up

---
+++ frontend/api/Api.tsx
@@ -0,0 +1,82 @@
+import { NewBookmarkRequest } from "@/components/bookmark/NewBookmarkCard";
+import axios from "axios";
+const SERVER_URL = "http://localhost:9000/api";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: SERVER_URL,
+  timeout: 3000,
+  transformResponse: [
+    function (data) {
+      return parseData(data);
+    },
+  ],
+});
+
+function parseData(data: string) {
+  return data ? JSON.parse(data) : data;
+}
+
+const api = {
+  async execute(
+    method: string,
+    resource: string,
+    data: any | null | undefined,
+    config: {} | undefined
+  ) {
+    return instance({
+      method: method,
+      url: resource,
+      data,
+    });
+  },
+
+  // (C)reate
+  addNewBookmark(newBkmkReq: NewBookmarkRequest) {
+    return this.execute("POST", "bookmark/add", newBkmkReq, {});
+  },
+  // (R)ead
+  getAllBookmarks() {
+    return this.execute("GET", "bookmarks", null, {});
+  },
+  bookmarkAddTagByTitle(bookmarkId: string | number, tagTitle: string) {
+    return this.execute("POST", "bookmark/" + bookmarkId + "/addTag", {
+      tag_title: tagTitle,
+    }, {});
+  },
+  bookmarkRemoveTagById(bookmarkId: string | number, tagId: number) {
+    return instance.delete("bookmark/" + bookmarkId + "/tagId", {
+      params: { id: tagId },
+    });
+  },
+  bookmarkRemoveTagByTitle(bookmarkId: string, title: any) {
+    return instance.delete("bookmark/" + bookmarkId + "/tagTitle", {
+      params: { title: title },
+    });
+  },
+  getAllTags() {
+    return this.execute("GET", "tagscnt", null, {});
+  },
+  addAllTag(strTags: string []) {
+    let t = JSON.stringify(strTags);
+    console.log(t);
+    return this.execute("POST", "tags/addTags", strTags, {});
+  },
+  getTagById(id: string) {
+    return this.execute("GET", "tags/id/" + id, null, {});
+  },
+  // (U)pdate
+  updateForId(id: string, text: any, completed: any) {
+    return this.execute("PUT", "bookmarks/" + id, {
+      title: text,
+      completed: completed,
+    }, {});
+  },
+
+  // (D)elete
+  removeBookmarkById(id: string) {
+    return this.execute("DELETE", "bookmarks/" + id, null, {});
+  },
+};
+
+export default api;
