b96a30754181a1d75e6eb0941ab07c4862d2cecf
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-12-18 14:46:39 -0500

    TestContainers work

---
+++ server/src/test/java/dev/findfirst/core/DatabaseTest.java
@@ -0,0 +1,39 @@
+package dev.findfirst.core;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import dev.findfirst.core.repository.BookmarkRepository;
+import dev.findfirst.security.userAuth.tenant.contexts.TenantContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DatabaseTest {
+
+  @MockBean private TenantContext tenantContext;
+
+  @Container @ServiceConnection
+  private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.4");
+
+  @Autowired BookmarkRepository bkmkRepo;
+
+  @Test
+  void connectionEstablish() {
+    assertThat(postgres.isCreated()).isTrue();
+  }
+
+  @Test
+  void repoLoads() {
+    assertNotNull(bkmkRepo);
+  }
+}
--- server/src/test/java/dev/findfirst/core/controller/UserControllerTest.java
+++ server/src/test/java/dev/findfirst/core/controller/UserControllerTest.java
@@ -1,6 +1,44 @@
 package dev.findfirst.core.controller;
 
-import dev.findfirst.core.annotations.IntegrationTestConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@IntegrationTestConfig
-public class UserControllerTest {}
+import dev.findfirst.core.repository.BookmarkRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DataJpaTest
+@Testcontainers
+@ActiveProfiles("postgres")
+public class UserControllerTest {
+
+  @Container
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+
+  @Autowired BookmarkRepository bkmkRepo;
+
+  // @Test
+  // public void userShouldHaveBookmarks() {
+  //   var results = bkmkRepo.findAll();
+  //   assertNotNull(results);
+  // }
+  @Test
+  void test() {
+    assertEquals(postgres.isRunning(), true);
+    // assertNotNull(bkmkRepo);
+  }
+}
