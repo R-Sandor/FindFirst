b6ee138bd5a671c508a49f87d4cc5f453d0bcaa3
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-12-22 09:40:56 -0500

    removing pointless test that will fail

--- server/src/test/java/dev/findfirst/core/controller/BookmarkControllerTest.java
+++ server/src/test/java/dev/findfirst/core/controller/BookmarkControllerTest.java
@@ -1,14 +1,18 @@
 package dev.findfirst.core.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import dev.findfirst.core.annotations.IntegrationTestConfig;
+import dev.findfirst.core.model.Bookmark;
 import dev.findfirst.core.repository.BookmarkRepository;
 import dev.findfirst.security.userAuth.tenant.contexts.TenantContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -23,11 +27,19 @@ public class BookmarkControllerTest {
   static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
   @Autowired BookmarkRepository bkmkRepo;
-  @MockBean private TenantContext tenantContext;
+
+  @MockBean TenantContext tenantContext;
+
+  @Autowired TestRestTemplate restTemplate;
 
   @Test
-  void test() {
+  void containerStarupTest() {
     assertEquals(postgres.isRunning(), true);
-    // assertNotNull(bkmkRepo);
+  }
+
+  @Test
+  void shouldFailBecauseNoOneIsAuthenticated() {
+    var bookmarks = restTemplate.getForObject("/api/bookmarks", Bookmark[].class);
+    assertEquals(0, bookmarks.length);
   }
 }
