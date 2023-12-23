488ada55557769ceadfd94d6139dd3309ccf9dc9
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-12-23 06:01:28 -0500

    UserManagement handles the JWT from signin

--- server/src/main/java/dev/findfirst/users/service/UserManagementService.java
+++ server/src/main/java/dev/findfirst/users/service/UserManagementService.java
@@ -1,10 +1,14 @@
 package dev.findfirst.users.service;
 
+import dev.findfirst.security.jwt.service.RefreshTokenService;
 import dev.findfirst.security.userAuth.execeptions.NoUserFoundException;
+import dev.findfirst.security.userAuth.models.RefreshToken;
 import dev.findfirst.security.userAuth.models.payload.request.SignupRequest;
 import dev.findfirst.security.userAuth.tenant.data.TenantService;
+import dev.findfirst.security.userAuth.utils.Constants;
 import dev.findfirst.users.exceptions.EmailAlreadyRegisteredException;
 import dev.findfirst.users.exceptions.UserNameTakenException;
+import dev.findfirst.users.model.user.SigninTokens;
 import dev.findfirst.users.model.user.Token;
 import dev.findfirst.users.model.user.URole;
 import dev.findfirst.users.model.user.User;
@@ -12,12 +16,18 @@ import dev.findfirst.users.repository.PasswordTokenRepository;
 import dev.findfirst.users.repository.RoleRepository;
 import dev.findfirst.users.repository.UserRepo;
 import dev.findfirst.users.repository.VerificationTokenRepository;
+import java.nio.charset.StandardCharsets;
 import java.rmi.UnexpectedException;
+import java.time.Instant;
+import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,9 +37,13 @@ public class UserManagementService {
   private final UserRepo userRepo;
   private final VerificationTokenRepository tokenRepository;
   private final PasswordTokenRepository passwordTokenRepository;
+  private final RefreshTokenService refreshTokenService;
   private final PasswordEncoder passwdEncoder;
   private final RoleRepository roleRepository;
   private final TenantService tenantService;
+  private final JwtEncoder encoder;
+
+  @Value("${findfirst.app.jwtExpirationMs}") private int jwtExpirationMs;
 
   public User getUserByEmail(String email) throws NoUserFoundException {
     return userRepo.findByEmail(email).orElseThrow(NoUserFoundException::new);
@@ -113,5 +127,41 @@ public class UserManagementService {
       deleteUser(user);
       throw new UnexpectedException("Unexpected error occured during signup, try again");
     }
+  }
+
+  public String generateTokenFromUser(User user) {
+    Instant now = Instant.now();
+    String email = user.getEmail();
+    Integer roleId = user.getRole().getId();
+    String roleName = user.getRole().getName().name();
+    Integer tenantId = user.getTenantId();
+    JwtClaimsSet claims =
+        JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(Instant.now())
+            .expiresAt(now.plusSeconds(jwtExpirationMs))
+            .subject(email)
+            .claim("scope", email)
+            .claim(Constants.ROLE_ID_CLAIM, roleId)
+            .claim(Constants.ROLE_NAME_CLAIM, roleName)
+            .claim(Constants.TENANT_ID_CLAIM, tenantId)
+            .build();
+    return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+  }
+
+  public SigninTokens signinUser(String authorization) throws NoUserFoundException {
+    String base64Credentials = authorization.substring("Basic".length()).trim();
+    byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+    String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+    // credentials = username:password
+    final String[] values = credentials.split(":", 2);
+
+    // This error should never occur, as authentication checks username and throws.
+    User user;
+    user = getUserByUsername(values[0]);
+    final RefreshToken refreshToken = refreshTokenService.createRefreshToken(user);
+    String jwt = generateTokenFromUser(user);
+
+    return new SigninTokens(jwt, refreshToken.getToken());
   }
 }
