2de351ae93f62a11dbb0772d63df6f691a8ddd69
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-01-02 06:56:41 -0500

    Yup validation added still need to add errors

--- frontend/__tests__/account/signup/signup.test.tsx
+++ frontend/__tests__/account/signup/signup.test.tsx
@@ -1,24 +1,38 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Page from "app/account/signup/page"
- 
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+import Page from "app/account/signup/page";
 
+
+const user = userEvent.setup()
 /**
- * User should be able to enter in the form. 
+ * Input forms should all be empty intially.
  */
 test("Forms allow input", () => {
-  render(<Page />)
-  const emailInputElement = screen.getByRole("textbox");
-  expect(emailInputElement.value).toBe("");
+  render(<Page />);
+  const form = screen.getAllByRole("textbox") as HTMLInputElement[];
+  expect(form[0].value).toBe("");
+  expect(form[1].value).toBe("");
+  const password = screen.getByPlaceholderText(/password/i) as HTMLInputElement;
+  expect(password.value).toBe("");
+});
 
-})
+test("Should be able to type an userName", () => {
+  render(<Page />);
+  const form = screen.getAllByRole("textbox") as HTMLInputElement[];
+  const usernameInput = form[0];
+  expect(form[0].value).toBe("");
+  const userName = "jsmith";
+  user.type(usernameInput, userName)
+  expect(usernameInput.value).toBe(userName);
+});
 
 // User should be able to enter in an email and it be valid
-  // OTHERWISE: ERROR, Submit is not allowed. 
-  /**
-   * enter in email in email box that is invalid
-   * assert that that error window or form is in error state. 
-   * assert that submit button is not enabled.
-   */
+// OTHERWISE: ERROR, Submit is not allowed.
+/**
+ * enter in email in email box that is invalid
+ * assert that that error window or form is in error state.
+ * assert that submit button is not enabled.
+ */
 
-  // 
+//
--- frontend/app/account/signup/page.tsx
+++ frontend/app/account/signup/page.tsx
@@ -1,69 +1,80 @@
 "use client";
 import { Formik, Field, Form } from "formik";
-import styles from "./signup-form.module.css";
-import  authService  from "@/services/auth.service";
+import styles from "./signup-form.module.scss";
+import authService from "@/services/auth.service";
 import { useRouter } from "next/navigation";
+import * as Yup from "yup";
 
 export interface signupRequest {
-  username: string;
+  userName: string;
+  email: string;
   password: string;
 }
 
-export default function Page() { 
+const SignupSchema = Yup.object().shape({
+  userName: Yup.string()
+    .min(4, "Too Short!")
+    .max(50, "Too long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+export default function Page() {
   const router = useRouter();
-  const handleOnSubmit = async (signupRequest: signupRequest, actions: any) => {
-    if (await authService.login(signupRequest)) {
-      router.push("/");
-    }
-  }
+  const handleOnSubmit = async (
+    signupRequest: signupRequest,
+    actions: any
+  ) => {};
   return (
     <div className="grid h-screen place-items-center">
-    <div className={ "content-center " + styles.login_box + " p-3"}>
-      <h1 className="display-6 mb-3">Sign up</h1>
-      <Formik
-        initialValues={{
-          username: "",
-          email: "",
-          password: "",
-        }}
-        onSubmit={handleOnSubmit}
-      >
-        <Form>
-          <div className="mb-3">
-            <Field
-              className="form-control"
-              id="username"
-              name="username"
-              placeholder="Username"
-              aria-describedby="usernameHelp"
-            />
-          </div>
+      <div className={"content-center " + styles.login_box + " p-3"}>
+        <h1 className="display-6 mb-3">Sign up</h1>
+        <Formik
+          initialValues={{
+            userName: "",
+            email: "",
+            password: "",
+          }}
+          onSubmit={handleOnSubmit}
+          validationSchema={SignupSchema}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className="mb-3">
+                <Field
+                  className="form-control"
+                  id="userName"
+                  name="userName"
+                  placeholder="Username"
+                  type="userName"
+                />
+              </div>
+              <div className="mb-3">
+                <Field
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                />
+              </div>
+              <div className="mb-3">
+                <Field
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                />
+              </div>
 
-          <div className="mb-3">
-            <Field
-              className="form-control"
-              id="email"
-              name="email"
-              placeholder="Email"
-              type="email"
-            />
-          </div>
-          <div className="mb-3">
-            <Field
-              className="form-control"
-              id="password"
-              name="password"
-              placeholder="Password"
-              type="password"
-            />
-          </div>
-
-          <button type="submit" className="btn">
-            Login
-          </button>
-        </Form>
-      </Formik>
+              <button type="submit" className={`btn ${styles.login_button}`}>
+                Login
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
