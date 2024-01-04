1ea689277620ca3aaaaa3ffffebc05aa0304b892
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-01-04 04:43:03 -0500

    "unused import"

--- server/src/main/java/dev/findfirst/core/controller/BookmarkController.java
+++ server/src/main/java/dev/findfirst/core/controller/BookmarkController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.function.Consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
