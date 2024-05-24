5d4aa558d4865a7e289bbba35bf0fd19a2de85a6
rsandor1 <45048465+R-Sandor@users.noreply.github.com>
2024-05-24 09:20:43 -0400

    unit testing

--- frontend/__tests__/BookmarkGroup/Bookmark/NewBookmarkCard.test.tsx
+++ frontend/__tests__/BookmarkGroup/Bookmark/NewBookmarkCard.test.tsx
@@ -1,10 +1,9 @@
-import { describe, expect, it, test, vi } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import NewBookmarkCard from "@components/BookmarkGroup/Bookmark/NewBookmarkCard";
-import authService, { User } from "@services/auth.service";
-import { instance } from "@api/Api";
-import Tag from "@type/Bookmarks/Tag";
 import { debug } from "vitest-preview";
+import userEvent from "@testing-library/user-event";
+const user = userEvent.setup();
 
 describe("New Bookmark Card Renders", () => {
   beforeEach(() => {
@@ -18,15 +17,44 @@ describe("New Bookmark Card Renders", () => {
   });
 
   it("Card renders", () => {
-    debug();
     expect(screen.getByText("Add Bookmark")).toBeDefined();
+    expect(screen.getByPlaceholderText(/discover/i)).toBeDefined();
+    expect(screen.getByPlaceholderText("Enter a tag")).toBeDefined();
+    expect(screen.getByText("Reset")).toBeDefined();
+    expect(screen.getByText("Submit")).toBeDefined();
   });
 });
 
 describe("All Fields Work", () => {
-  it("No fields have data, should be disabled", () => {});
+  beforeEach(() => {
+    render(
+      <div data-bs-theme="dark" className="row pt-3">
+        <div className="col-6 col-sm-12 col-md-12 col-lg-4">
+          <NewBookmarkCard />
+        </div>
+      </div>,
+    );
+  });
 
-  it("All fields are given data", () => {});
+  it("No fields have data, should be disabled", () => {
+    const submit = screen.getByText("Submit");
+    expect(submit).toBeDisabled();
+  });
+
+  it("All Required fields are given data", async () => {
+    await user.type(screen.getByPlaceholderText(/discover/i), "facebook.com");
+    const submit = screen.getByText("Submit");
+    expect(submit).not.toBeDisabled();
+  });
+
+  it("Is a valid domain", async () => {
+    await user.type(
+      screen.getByPlaceholderText(/discover/i),
+      "facebook.whatever",
+    );
+    const submit = screen.getByText("Submit");
+    expect(submit).toBeDisabled();
+  });
 
   it("Reset option works", () => {});
 
