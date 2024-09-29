3e0b45f9af33247891378a375538e33e8ba43694
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-09-29 06:17:34 -0400

    Screenshots served from Spring Boot server

--- server/src/main/java/dev/findfirst/core/controller/ImageController.java
+++ server/src/main/java/dev/findfirst/core/controller/ImageController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ImageController {
 
+  @Value("${findfirst.screenshot.location}") private String screenshotSaveLoc;
+
   @GetMapping("{fileName}")
   public byte[] getImage(@PathVariable String fileName) {
-
-    Path filePath = Path.of("/app/screenshots/" + fileName);
-    try {
-      return Files.newInputStream(filePath, StandardOpenOption.READ).readAllBytes();
-    } catch (IOException ex) {
-      log.error(ex.toString());
-      return new byte[0];
+    Path filePath = Path.of(screenshotSaveLoc, fileName);
+    if (Files.exists(filePath)) {
+      try {
+        return Files.newInputStream(filePath, StandardOpenOption.READ).readAllBytes();
+      } catch (IOException ex) {
+        log.error(ex.toString());
+        return new byte[0];
+      }
     }
+    return new byte[0];
   }
 }
