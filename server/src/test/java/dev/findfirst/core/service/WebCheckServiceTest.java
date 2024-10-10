944c4fb19f411877059d33d991feb70124a7dc0f
joelramilison <joel.ramilison@gmail.com>
2024-10-10 21:57:12 +0200

    (Scaffolding) new WebCheckService checks robots.txt for scrape permission

---
+++ server/src/test/java/dev/findfirst/core/service/WebCheckServiceTest.java
@@ -0,0 +1,24 @@
+package dev.findfirst.core.service;
+
+import org.junit.Test;
+
+public class WebCheckServiceTest {
+
+  WebCheckService webCheckService;
+
+  public WebCheckServiceTest() {
+    webCheckService = new WebCheckService();
+  }
+
+  @Test
+  public void isScrapable() {
+
+  }
+
+  @Test
+  public void getRobotsTxt() {
+
+  }
+
+
+}
