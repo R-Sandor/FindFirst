30134a2f277aa21d30f5f4949153b017a48d0a25
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-10-23 16:48:44 -0400

    Add TagRepository for JDBC

---
+++ server/src/main/java/dev/findfirst/core/repository/TagJDBCRepository.java
@@ -0,0 +1,8 @@
+package dev.findfirst.core.repository;
+
+import dev.findfirst.core.model.TagJDBC;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TagJDBCRepository extends CrudRepository<TagJDBC, Long> {
+}
