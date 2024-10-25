5b05c28b6a44fd3de192a2c9616946dc24767b5f
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-10-25 08:40:11 -0400

    update test data

---
+++ server/src/main/resources/db/migration/v1.0.4__update_test_data.sql
@@ -0,0 +1,15 @@
+UPDATE tag
+SET created_date = CURRENT_DATE
+WHERE created_date IS NULL;
+
+UPDATE tag
+SET last_modified_date = CURRENT_DATE
+WHERE last_modified_date IS NULL;
+
+UPDATE tag
+SET last_modified_by = "system"
+WHERE last_modified_by IS NULL;
+
+UPDATE tag
+SET created_by = "system"
+WHERE created_by IS NULL;
