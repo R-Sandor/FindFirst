35ac8e14f1cc6b428f95620b33a9947cb2882fcc
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-11-16 05:03:50 -0500

    add Tag to a new bookmark

--- server/src/test/java/dev/findfirst/core/DatabaseTest.java
+++ server/src/test/java/dev/findfirst/core/DatabaseTest.java
@@ -83,17 +83,21 @@ public class DatabaseTest {
   @Test()
   @Order(2)
   void addTagToBookmark() {
-    int tenantId = 1;
-    var bookmark = new BookmarkJDBC(null, tenantId, new Date(), "test", "test", new Date(),
+    int userId = 1;
+    var bookmark = new BookmarkJDBC(null, userId, new Date(), "test", "test", new Date(),
         "My very cool bookmark", "https://test.com", "test.com", true, new HashSet<>());
     var bkmkEnt = bkmkJDBCRepo.save(bookmark);
 
-    var tagEnt = tagRepo.save(new TagJDBC(null, tenantId, new Date(System.currentTimeMillis()),
+    var tagEnt = tagRepo.save(new TagJDBC(null, userId, new Date(System.currentTimeMillis()),
         "test", "test", new Date(System.currentTimeMillis()), "TestTAG"));
 
     BookmarkTag bookmarkTag = new BookmarkTag(bkmkEnt.getId(), tagEnt.getId());
 
     bookmarkTagRepository.saveBookmarkTag(bookmarkTag);
+
+    var tagSet = bookmarkTagRepository.getAllTagIdsForBookmark(bkmkEnt.getId(), userId);
+    assertEquals(1, tagSet.size());
+
   }
 
 }
