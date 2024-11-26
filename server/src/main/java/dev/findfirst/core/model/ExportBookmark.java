c5c6383855634bebdc0680b58dd9c3d620d0ea22
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-11-26 04:26:06 -0500

    Replace size check with isEmpty()

--- server/src/main/java/dev/findfirst/core/model/ExportBookmark.java
+++ server/src/main/java/dev/findfirst/core/model/ExportBookmark.java
@@ -7,7 +7,6 @@ import dev.findfirst.core.dto.BookmarkOnly;
 
 import lombok.Data;
 
-
 /** Takes bookmarks and turns them into an html file in the form of a String. */
 @Data
 public class ExportBookmark {
@@ -20,18 +19,17 @@ public class ExportBookmark {
   private static String endOfContent = "\n</DL>";
 
   public ExportBookmark(List<TagBookmarks> tagBookmarks) {
-    String defaultText =
-        """
-            <!DOCTYPE NETSCAPE-Bookmark-file-1>
-            <!-- This is an automatically generated file.
-            It will be read and overwritten.
-            DO NOT EDIT! -->
-            <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-            <meta http-equiv="Content-Security-Policy"  content="default-src 'self'; script-src 'none'; img-src data: *; object-src 'none'"></meta>
-            <TITLE>FindFirst Bookmarks</TITLE>
-            <H1>Bookmarks</H1>
-            <DL><p>
-            """;
+    String defaultText = """
+        <!DOCTYPE NETSCAPE-Bookmark-file-1>
+        <!-- This is an automatically generated file.
+        It will be read and overwritten.
+        DO NOT EDIT! -->
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Security-Policy"  content="default-src 'self'; script-src 'none'; img-src data: *; object-src 'none'"></meta>
+        <TITLE>FindFirst Bookmarks</TITLE>
+        <H1>Bookmarks</H1>
+        <DL><p>
+        """;
     htmlOut = new StringBuilder(defaultText);
     content = tagBookmarks;
     buildHtml();
@@ -49,7 +47,7 @@ public class ExportBookmark {
   private String getCat(TagBookmarks tb) {
     Date date = null;
     Long time = 0l;
-    if (tb.bookmarks().size() > 0) {
+    if (!tb.bookmarks().isEmpty()) {
       date = tb.bookmarks().get(0).createdDate();
     }
     if (date != null) {
