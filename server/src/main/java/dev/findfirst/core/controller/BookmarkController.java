aae58477f6fc856884bf950d687ffe31b29659e1
Ali Rahbar <alirahbar2005@gmail.com>
2025-01-02 11:40:03 -0500

    Fixed Code Style

--- server/src/main/java/dev/findfirst/core/controller/BookmarkController.java
+++ server/src/main/java/dev/findfirst/core/controller/BookmarkController.java
@@ -46,50 +46,43 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
-public class BookmarkController
-{
+public class BookmarkController {
 
   private final BookmarkService bookmarkService;
 
   private final TagService tagService;
 
   @GetMapping("/bookmarks")
-  public ResponseEntity<List<BookmarkDTO>> getAllBookmarks()
-  {
+  public ResponseEntity<List<BookmarkDTO>> getAllBookmarks() {
     return new Response<>(bookmarkService.listJDBC(), HttpStatus.OK).get();
   }
 
   @GetMapping(value = "/bookmarks/export", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-  public ResponseEntity<byte[]> exportAllBookmarks()
-  {
+  public ResponseEntity<byte[]> exportAllBookmarks() {
     return ResponseEntity.ok()
             .header("Content-Disposition", "attachment; filename=findfirst-bookmarks.html")
             .body(bookmarkService.export().getBytes());
   }
 
   @DeleteMapping(value = "/bookmarks")
-  public ResponseEntity<String> deleteAll()
-  {
+  public ResponseEntity<String> deleteAll() {
     bookmarkService.deleteAllBookmarks();
     return ResponseEntity.ok("Deleted All user's bookmarks");
   }
 
   @GetMapping(value = "/bookmark")
-  public ResponseEntity<BookmarkDTO> getBookmarkById(@RequestParam("id") long id)
-  {
+  public ResponseEntity<BookmarkDTO> getBookmarkById(@RequestParam("id") long id) {
     return new Response<>(bookmarkService.getBookmarkDTOById(id)).get();
   }
 
   @ExceptionHandler(TagNotFoundException.class)
-  public ResponseEntity<String> handle()
-  {
+  public ResponseEntity<String> handle() {
     return ResponseEntity.internalServerError().body(new TagNotFoundException().getMessage());
   }
 
   @PostMapping("/bookmark")
   public ResponseEntity<BookmarkDTO> addBookmark(@RequestBody AddBkmkReq req)
-          throws TagNotFoundException, URISyntaxException
-  {
+          throws TagNotFoundException, URISyntaxException {
     var response = new Response<BookmarkDTO>();
     BookmarkDTO createdBookmark = bookmarkService.addBookmark(req);
     return response.setResponse(createdBookmark, HttpStatus.OK);
@@ -104,15 +97,13 @@ public class BookmarkController
    */
   @PatchMapping("/bookmark")
   public ResponseEntity<BookmarkDTO> updateBookmark(
-          @Valid @RequestBody UpdateBookmarkReq updateBookmarkReq) throws BookmarkNotFoundException
-  {
+          @Valid @RequestBody UpdateBookmarkReq updateBookmarkReq) throws BookmarkNotFoundException {
     return new ResponseEntity<>(bookmarkService.updateBookmark(updateBookmarkReq), HttpStatus.OK);
   }
 
   @DeleteMapping("/bookmark")
   public ResponseEntity<String> deleteById(@RequestParam("id") Long id)
-          throws JsonProcessingException
-  {
+          throws JsonProcessingException {
     bookmarkService.deleteBookmark(id);
     ObjectMapper mapper = new ObjectMapper();
     String jsonStr = mapper.writeValueAsString("Deleted Bookmark %s".formatted(id));
@@ -121,16 +112,14 @@ public class BookmarkController
 
   @PostMapping("/bookmark/addBookmarks")
   public ResponseEntity<List<BookmarkDTO>> addBookmarks(
-          @RequestBody @Size(min = 1, max = 100) List<AddBkmkReq> bookmarks)
-  {
+          @RequestBody @Size(min = 1, max = 100) List<AddBkmkReq> bookmarks) {
     return new ResponseEntity<>(bookmarkService.addBookmarks(bookmarks), HttpStatus.OK);
   }
 
   @PostMapping("/bookmark/{bookmarkID}/tag")
   public ResponseEntity<TagDTO> addTag(@PathVariable(value = "bookmarkID") @NotNull long bookmarkId,
                                        @RequestParam("tag") @Size(max = 512) @NotBlank String title)
-          throws BookmarkNotFoundException
-  {
+          throws BookmarkNotFoundException {
     final var bkmkOpt = bookmarkService.findByIdJDBC(bookmarkId);
 
     var bookmark = bkmkOpt.orElseThrow(BookmarkNotFoundException::new);
@@ -148,8 +137,7 @@ public class BookmarkController
   @PostMapping("/bookmark/{bookmarkID}/tagId")
   public ResponseEntity<BookmarkDTO> addTagById(@PathVariable(value = "bookmarkID") Long bookmarkId,
                                                 @RequestParam(value = "tagId") Long tagId)
-          throws BookmarkNotFoundException, TagNotFoundException
-  {
+          throws BookmarkNotFoundException, TagNotFoundException {
 
     var bookmark =
             bookmarkService.findByIdJDBC(bookmarkId).orElseThrow(BookmarkNotFoundException::new);
@@ -159,8 +147,7 @@ public class BookmarkController
   }
 
   @ExceptionHandler(BookmarkNotFoundException.class)
-  public ResponseEntity<String> handleNotFoundException()
-  {
+  public ResponseEntity<String> handleNotFoundException() {
     return ResponseEntity.internalServerError().body(new BookmarkNotFoundException().getMessage());
   }
 
@@ -168,8 +155,7 @@ public class BookmarkController
   public ResponseEntity<BookmarkTag> deleteTagFromBookmark(
           @Valid @PathVariable("bookmarkID") long bookmarkID,
           @RequestParam("tag") @NotBlank String title)
-          throws TagNotFoundException, BookmarkNotFoundException
-  {
+          throws TagNotFoundException, BookmarkNotFoundException {
 
     var t = tagService.findIdByTagTitleJDBC(title).orElseThrow(TagNotFoundException::new);
     // verify that the bookmark exists.
@@ -182,8 +168,7 @@ public class BookmarkController
   @DeleteMapping("bookmark/{bookmarkID}/tagId")
   public ResponseEntity<BookmarkTag> deleteTagFromBookmarkById(
           @Valid @PathVariable("bookmarkID") long bookmarkID, @RequestParam("tagId") @Valid long tagId)
-          throws TagNotFoundException, BookmarkNotFoundException
-  {
+          throws TagNotFoundException, BookmarkNotFoundException {
 
     var t = tagService.findByIdJDBC(tagId).orElseThrow(TagNotFoundException::new);
     // verify that the bookmark exists.
@@ -195,8 +180,7 @@ public class BookmarkController
 
   @PostMapping(value = "bookmark/import", produces = MediaType.APPLICATION_NDJSON_VALUE)
   public Flux<BookmarkDTO> importBookmarks(@RequestParam("file") MultipartFile file)
-          throws IOException
-  {
+          throws IOException {
     // Check file size
     final long MAX_FILE_SIZE = 250L * 1_000_000 / 8; // 250 Mb
     if (file.getSize() > MAX_FILE_SIZE) {
