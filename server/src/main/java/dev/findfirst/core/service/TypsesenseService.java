9ff41e0bf3926d8100b6664619f6f20cb2db2201
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-01-06 07:04:30 -0500

    Service/test scaffolding

---
+++ server/src/main/java/dev/findfirst/core/service/TypsesenseService.java
@@ -0,0 +1,10 @@
+package dev.findfirst.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TypsesenseService { 
+
+  
+
+}
---
+++ server/src/test/java/dev/findfirst/core/service/TypesenseServiceTest.java
@@ -0,0 +1,9 @@
+package dev.findfirst.core.service;
+
+
+class TypesenseServiceTest { 
+
+  // Mock checking the repo for record
+  // Use the file to test typesense service.
+  // Mock saving the initialized file. 
+}
