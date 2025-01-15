1511bdb832c2201a830317f2f36819a6d40ae017
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-01-15 05:53:01 -0500

    Fix cross platform copy file issues for dev. (#307)
    While not perfect to use Java to copy a file.
    Butveryone working on findfirst should have Java installed
    and I don't need to introduce any dependencies to my package.json/pnpm-lock.
    
    Closes #300

---
+++ frontend/CpEnv.java
@@ -0,0 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+public class CpEnv { 
+  public static void main(String [] args) throws Exception { 
+    Files.copy(Path.of(args[0]), Path.of(args[1]), StandardCopyOption.valueOf("REPLACE_EXISTING"));
+  }
+}
