c6334389d4d9345a80ce747c2757862637d0ecb7
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-01-26 06:04:28 -0500

    Fix code smell

--- frontend/pages/api/next/screenshot/[screenshot].ts
+++ frontend/pages/api/next/screenshot/[screenshot].ts
@@ -4,19 +4,24 @@ import fs from "fs";
 import path from "path";
 import axios from "axios";
 
-const IMAGE_DIR = process.env.NEXT_PUBLIC_IMAGE_DIR;
+const IMAGE_DIR = getImageDir();
 const SERVED_IMAGE = process.env.NEXT_PUBLIC_SERVER_URL + "/api/screenshots/";
+
+function getImageDir(): string {
+  if (process.env.NEXT_PUBLIC_IMAGE_DIR) {
+    return process.env.NEXT_PUBLIC_IMAGE_DIR;
+  }
+  // use the dev default if not set.
+  return "../data/screenshots/";
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { screenshot } = req.query;
 
-  const publicPath = path.join(
-    process.cwd(),
-    IMAGE_DIR ? IMAGE_DIR : "../data/screenshots/",
-    screenshot as string,
-  );
+  const publicPath = path.join(process.cwd(), IMAGE_DIR, screenshot as string);
 
   if (fs.existsSync(publicPath) && screenshot) {
     fs.readFile(publicPath, (err, data) => {
