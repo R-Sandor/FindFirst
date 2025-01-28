6bd91a8cef21b93a264edaaba7f20abadce953e7
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-01-28 05:14:16 -0500

    Fix fulltext search - broken host parameters

--- server/src/main/java/dev/findfirst/core/controller/SearchController.java
+++ server/src/main/java/dev/findfirst/core/controller/SearchController.java
@@ -13,7 +13,6 @@ import dev.findfirst.core.utilies.Response;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
