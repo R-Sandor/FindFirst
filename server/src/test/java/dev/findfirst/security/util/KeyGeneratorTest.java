81d6eb32b18866dd622b6573611b278e005cb936
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-02-11 04:50:18 -0500

    Add main key gen test too

--- server/src/test/java/dev/findfirst/security/util/KeyGeneratorTest.java
+++ server/src/test/java/dev/findfirst/security/util/KeyGeneratorTest.java
@@ -9,8 +9,12 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
+@ExtendWith(OutputCaptureExtension.class)
 class KeyGeneratorTest {
 
   @TempDir
@@ -24,17 +28,19 @@ class KeyGeneratorTest {
     KeyGenerator.generateKeys(privateKeyPath.toString(), publicKeyPath.toString());
 
     String pubKeyContent = Files.readString(publicKeyPath);
-    assertTrue(pubKeyContent.contains("-----BEGIN PUBLIC KEY-----"), "Public key should start with PEM header");
-    assertTrue(pubKeyContent.contains("-----END PUBLIC KEY-----"), "Public key should end with PEM footer");
+    assertTrue(pubKeyContent.contains("-----BEGIN PUBLIC KEY-----"),
+        "Public key should start with PEM header");
+    assertTrue(pubKeyContent.contains("-----END PUBLIC KEY-----"),
+        "Public key should end with PEM footer");
 
     String privKeyContent = Files.readString(privateKeyPath);
-    assertTrue(privKeyContent.contains("-----BEGIN PRIVATE KEY-----"), "Private key should start with PEM header");
-    assertTrue(privKeyContent.contains("-----END PRIVATE KEY-----"), "Private key should end with PEM footer");
+    assertTrue(privKeyContent.contains("-----BEGIN PRIVATE KEY-----"),
+        "Private key should start with PEM header");
+    assertTrue(privKeyContent.contains("-----END PRIVATE KEY-----"),
+        "Private key should end with PEM footer");
 
-    String pubKeyBase64 = pubKeyContent
-        .replace("-----BEGIN PUBLIC KEY-----", "")
-        .replace("-----END PUBLIC KEY-----", "")
-        .replaceAll("\\s", ""); 
+    String pubKeyBase64 = pubKeyContent.replace("-----BEGIN PUBLIC KEY-----", "")
+        .replace("-----END PUBLIC KEY-----", "").replaceAll("\\s", "");
     byte[] pubKeyBytes = Base64.getDecoder().decode(pubKeyBase64);
     assertTrue(pubKeyBytes.length > 0, "Decoded public key should not be empty");
   }
@@ -48,33 +54,42 @@ class KeyGeneratorTest {
 
     KeyGenerator.generateKeys(privateKeyPath.toString(), publicKeyPath.toString());
 
-    assertEquals("dummy private key", Files.readString(privateKeyPath), "Private key file should not be overwritten");
-    assertEquals("dummy public key", Files.readString(publicKeyPath), "Public key file should not be overwritten");
+    assertEquals("dummy private key", Files.readString(privateKeyPath),
+        "Private key file should not be overwritten");
+    assertEquals("dummy public key", Files.readString(publicKeyPath),
+        "Public key file should not be overwritten");
   }
 
   @Test
-  void testGenerateKeysProducesDifferentKeysOnSeparateRuns() throws NoSuchAlgorithmException, IOException {
+  void testGenerateKeysProducesDifferentKeysOnSeparateRuns()
+      throws NoSuchAlgorithmException, IOException {
     Path privateKeyPath1 = tempDir.resolve("test1.key");
     Path publicKeyPath1 = tempDir.resolve("test1.pub");
     Path privateKeyPath2 = tempDir.resolve("test2.key");
     Path publicKeyPath2 = tempDir.resolve("test2.pub");
-    
+
     KeyGenerator.generateKeys(privateKeyPath1.toString(), publicKeyPath1.toString());
     KeyGenerator.generateKeys(privateKeyPath2.toString(), publicKeyPath2.toString());
 
     String pubKeyContent1 = Files.readString(publicKeyPath1);
     String pubKeyContent2 = Files.readString(publicKeyPath2);
 
-    String pubKeyBase64_1 = pubKeyContent1
-        .replace("-----BEGIN PUBLIC KEY-----", "")
-        .replace("-----END PUBLIC KEY-----", "")
-        .replaceAll("\\s", "");
-    String pubKeyBase64_2 = pubKeyContent2
-        .replace("-----BEGIN PUBLIC KEY-----", "")
-        .replace("-----END PUBLIC KEY-----", "")
-        .replaceAll("\\s", "");
+    String pubKeyBase64_1 = pubKeyContent1.replace("-----BEGIN PUBLIC KEY-----", "")
+        .replace("-----END PUBLIC KEY-----", "").replaceAll("\\s", "");
+    String pubKeyBase64_2 = pubKeyContent2.replace("-----BEGIN PUBLIC KEY-----", "")
+        .replace("-----END PUBLIC KEY-----", "").replaceAll("\\s", "");
 
     assertNotEquals(pubKeyBase64_1, pubKeyBase64_2, "Each generated public key should be unique");
 
+  }
+
+  @Test
+  void main(CapturedOutput output) throws IOException {
+    Files.delete(Path.of("src/main/resources/app.key"));
+    Files.delete(Path.of("src/main/resources/app.pub"));
+    KeyGenerator.main(null);
+    assertTrue(output.toString().contains("Keys Generated"));
+    KeyGenerator.main(null);
+    assertTrue(output.toString().contains("Keys already exist"));
   }
 }
