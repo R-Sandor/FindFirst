185a7c93a5ab3aaf0d0180f48da7899f834a29f6
R-Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-04-29 16:29:15 -0400

    adjust timeout

--- frontend/tests_e2e/setup.ts
+++ frontend/tests_e2e/setup.ts
@@ -1,13 +1,23 @@
-import { DockerComposeEnvironment, StartedDockerComposeEnvironment, Wait } from "testcontainers";
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+  Wait,
+} from "testcontainers";
 
-const composeFilePath = '../'
-const composeFile = 'docker-compose.yml'
+const composeFilePath = "../";
+const composeFile = "docker-compose.yml";
 
 export let composeEnv: StartedDockerComposeEnvironment;
 
 export default async function setup() {
- composeEnv = await new DockerComposeEnvironment(composeFilePath, composeFile)
-      .withWaitStrategy("server-1", Wait.forLogMessage("Started FindFirstApplication"))
-      .withWaitStrategy("frontend-1", Wait.forLogMessage("Ready in"))
-      .up()
+  console.log("spinning up docker");
+  composeEnv = await new DockerComposeEnvironment(composeFilePath, composeFile)
+    .withWaitStrategy(
+      "server-1",
+      Wait.forLogMessage("Started FindFirstApplication"),
+    )
+    .withWaitStrategy("frontend-1", Wait.forLogMessage("Ready in"))
+    .withWaitStrategy("screenshot-1", Wait.forListeningPorts())
+    // .withStartupTimeout(120_000) // Extend timeout to 120 seconds
+    .up();
 }
