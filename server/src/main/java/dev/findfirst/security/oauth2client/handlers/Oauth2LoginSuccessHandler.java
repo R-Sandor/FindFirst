fae7013fe1e7714270087564df0d23ba32a3d3f7
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-05-22 06:05:25 -0400

    server support for Oauth2

--- server/src/main/java/dev/findfirst/security/oauth2client/handlers/Oauth2LoginSuccessHandler.java
+++ server/src/main/java/dev/findfirst/security/oauth2client/handlers/Oauth2LoginSuccessHandler.java
@@ -1,26 +1,23 @@
 package dev.findfirst.security.oauth2client.handlers;
 
-
-
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
-import dev.findfirst.security.jwt.service.TokenService;
 import dev.findfirst.security.jwt.service.RefreshTokenService;
+import dev.findfirst.security.jwt.service.TokenService;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import dev.findfirst.users.model.user.SigninTokens;
-import dev.findfirst.users.service.UserManagementService;
 
 @Component
 @Slf4j
@@ -28,29 +25,33 @@ import dev.findfirst.users.service.UserManagementService;
 public class Oauth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
   @Value("${findfirst.app.frontend-url}")
-  private String redirectURL; 
+  private String redirectURL;
+
+  @Value("${findfirst.app.domain}")
+  private String domain;
+
+  @Value("${findfirst.secure-cookies:true}")
+  private boolean secure;
 
   private final TokenService ts;
-  private final RefreshTokenService rt; 
+  private final RefreshTokenService rt;
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws ServletException, IOException {
-    log.debug("successful login");
-  // ResponseCookie cookie = ResponseCookie.from("findfirst", tkns.jwt()).secure(secure).path("/")
-  //       .domain(domain).httpOnly(true).build();
-  //
-  //   return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
-  //       .body(new TokenRefreshResponse(tkns.refreshToken()));
-  authentication.getPrincipal();
-  DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
-  var userID = (Integer) principal.getAttributes().get("userID");
-  log.debug("userID {}", userID);
-  ts.generateTokenFromUser(userID);
 
-     // var signinTokens = new SigninTokens(jwt, refreshToken.getToken());
-    //
-  
+    DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
+    var userID = (Integer) principal.getAttributes().get("userID");
+
+    var jwt = ts.generateTokenFromUser(userID);
+
+    ResponseCookie cookie = ResponseCookie.from("findfirst", jwt).secure(secure).path("/")
+        .domain(domain).httpOnly(true).build();
+
+    response.addHeader("Set-Cookie", cookie.toString());
+    response.getWriter().write("""
+          { refreshToken: %s}
+        """.formatted(rt.createRefreshToken(userID)));
     getRedirectStrategy().sendRedirect(request, response, redirectURL);
 
   }
