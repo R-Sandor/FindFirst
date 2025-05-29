68fd675df0701f98c5a46fcb95bb107835ddfe31
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-05-29 04:52:39 -0400

    frontend to handle oauth2 signin

---
+++ frontend/app/account/login/oauth2/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+/**
+ *  Once a user logins with OAuth2 from backend service
+ *  they should be brought here. This will quickly read
+ *  the call authService which will make a request to 
+ *  the endpoint /user-info with the already attached 
+ *  cookie and save the User info.
+ */
+import authService from "@/services/auth.service";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function OauthLogin() {
+  const router = useRouter();
+
+  useEffect(() => {
+    authService.getUserInfoOauth2().then((user) => {
+      if (user) {
+        router.push("/");
+      }
+    })
+}, [authService])
+
+}
