62e32944759a39762c5e62e59d31b992cad03af2
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-10-06 05:42:04 -0400

    fix SQ finding about pattern matching

--- server/src/main/java/dev/findfirst/core/model/SearchBkmkByTagReq.java
+++ server/src/main/java/dev/findfirst/core/model/SearchBkmkByTagReq.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record SearchBkmkByTagReq(@NotEmpty List<String>tags){
+public record SearchBkmkByTagReq(@NotEmpty List<String> tags) {
 
-@Override public boolean equals(Object obj){if(obj instanceof SearchBkmkByTagReq tagSearch){return this.tags().equals(tagSearch.tags());}return false;}
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof SearchBkmkByTagReq(List<String> t)) {
+      return this.tags().equals(t);
+    }
+    return false;
+  }
 
 }
