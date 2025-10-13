562dde23039cf98d8eb68c0f5e0020f92199b0e4
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-10-13 05:26:27 -0400

    Fix import to jupiter

--- server/src/test/java/dev/findfirst/core/model/SearchBkmkByTagReqTest.java
+++ server/src/test/java/dev/findfirst/core/model/SearchBkmkByTagReqTest.java
@@ -1,7 +1,7 @@
 package dev.findfirst.core.model;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
