bc19a0f0ec8397d1dcad5467935b30c52e9b6ee6
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-10-28 06:42:10 -0400

    test for RobotsFetcher

---
+++ server/src/test/java/dev/findfirst/core/service/RobotsFetcherTest.java
@@ -0,0 +1,40 @@
+package dev.findfirst.core.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import dev.findfirst.core.service.RobotsFetcher.RobotsTxtResponse;
+
+class RobotsFetcherTest {
+
+  @Test
+  void testGettingRobotsTxt() {
+    RestTemplate restMock = Mockito.mock(RestTemplate.class);
+    byte[] expectedContent = "User-agent: *\nDisallow: /admin".getBytes();
+    String domain = "https://findfirst.com/robots.txt";
+
+    var expected = new RobotsTxtResponse(200, expectedContent, MediaType.TEXT_PLAIN.toString());
+    RobotsFetcher instance = new RobotsFetcher(restMock);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.TEXT_PLAIN);
+    ResponseEntity<String> mockReturn = new ResponseEntity<String>(new String(expectedContent), headers, HttpStatus.OK);
+
+    when(restMock.getForEntity(any(URI.class), eq(String.class))).thenReturn(mockReturn);
+    RobotsTxtResponse rTxtResponse = instance.getRobotsTxt(domain);
+    assertEquals(expected, rTxtResponse);
+  }
+}
