dcc31428c043ff0bcbb8dfff12187631d352a84f
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-11-08 04:48:56 -0500

    no need to specify keyfiles

--- server/scripts/entrypoint.sh
+++ server/scripts/entrypoint.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-mkdir keys
-./scripts/createServerKeys.sh /app/keys
-exec java -Djwt.private.key=file:/app/keys/app.key  -Djwt.public.key=file:/app/keys/app.pub -jar findfirst.jar
+exec java -jar findfirst.jar
+
