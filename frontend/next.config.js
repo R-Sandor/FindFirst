cc1ced06eedce9248b2b80e03368024ab9e7afe5
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-12-23 06:16:29 -0500

    for now silence the deprecation issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  // https://github.com/twbs/bootstrap/issues/40962#issuecomment-3436817277
+  sassOptions: {
+    silenceDeprecations: [
+      "mixed-decls",
+      "color-functions",
+      "global-builtin",
+      "import",
+    ],
+  },
 };
 
 module.exports = nextConfig;
