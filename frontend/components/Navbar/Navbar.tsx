344a38dc6b6770942c83f302cb915cd6c5e30902
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2026-01-06 06:09:22 -0500

    set person to generic person icon

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -109,18 +109,26 @@ const GlobalNavbar: React.FC = () => {
               <Export data-testid="export-component" />
             ) : null}
             <LightDarkToggle />
-            {userAuth === AuthStatus.Authorized && (
-              <Image
-                src={
-                  user?.profileImage && user?.profileImage.trim() !== ""
-                    ? `/api/user/avatar?userId=${user.id}`
-                    : "/img_avatar.png"
-                }
-                alt="Profile"
-                width={39}
-                height={39}
-              />
-            )}
+            {userAuth === AuthStatus.Authorized &&
+              (user?.profileImage && user?.profileImage.trim() !== "" ? (
+                <Image
+                  src={`/api/user/avatar?userId=${user.id}`}
+                  alt="Profile"
+                  width={39}
+                  height={39}
+                />
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="39"
+                  height="39"
+                  fill="currentColor"
+                  className="bi bi-file-person-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11" />
+                </svg>
+              ))}
             {authButton()}
           </div>
         </Navbar.Collapse>
